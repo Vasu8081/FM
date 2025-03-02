5312f408834b3ef2f02a291f35e64610cd50e96d
vasu8081 <vasudhanvarma@gmail.com>
2025-03-02 13:21:41 +0530

    added display

--- src/include/ui/home.hpp
+++ src/include/ui/home.hpp
@@ -14,6 +14,19 @@ public:
 #endif
         SetMinSize(wxSize(800, 600));
         SetMaxSize(wxSize(800, 600));
+        wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+        wxStaticText* title = new wxStaticText(this, wxID_ANY, "Home", wxPoint(10, 10), wxSize(100, 20));
+        sizer->Add(title, 0, wxEXPAND | wxALL, 10);
+        for (auto account: _models.getAccounts()) {
+            wxStaticText* account_text = new wxStaticText(this, wxID_ANY, account.second->to_str(), wxPoint(10, 10), wxSize(100, 20));
+            sizer->Add(account_text, 0, wxEXPAND | wxALL, 10);
+        }
+
+        for (auto transaction: _models.getTransactions()) {
+            wxStaticText* transaction_text = new wxStaticText(this, wxID_ANY, transaction->to_str(), wxPoint(10, 10), wxSize(100, 20));
+            sizer->Add(transaction_text, 0, wxEXPAND | wxALL, 10);
+        }
+        SetSizer(sizer);
         Centre();
     }
 private:
